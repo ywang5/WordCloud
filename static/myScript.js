0657c5e429d7c132d7ea04bda066510c3da3a10b
ywang5 <yikai.wang.1993@gmail.com>
2014-06-23 16:36:24 -0400

    Write each word to HTML as a separate div.

--- static/myScript.js
+++ static/myScript.js
@@ -1,8 +1,43 @@
 function FirstFunction(s) {
+    var max = getMax(s);
+    var min = getMin(s);
     var x;
-    var txt = "";
+    var font;
     for (x in s) {
-    	txt += x + ": " + s[x] + "<br>";
+    	//Create a seperate element div for each word, and assign it id
+    	var newElement = document.createElement('div');
+    	newElement.id = x;
+    	//Append the word to the element
+    	var textnode=document.createTextNode(x);
+    	newElement.appendChild(textnode);
+    	//Output to HTML
+    	document.body.appendChild(newElement);
+    	//Set the font size
+    	/*
+    	NOW NEED TO CHANGE THE FONT DYNAMICALLY
+    	*/
+    	document.getElementById(x).style.fontSize = "200%";
     }
-    document.getElementById("words").innerHTML = txt;
+}
+
+function getMax(s) {
+	var x;
+	var max = 1;
+	for (x in s) {
+		if (s[x] > max) {
+			max = s[x];
+		}
+	}
+	return max;
+}
+
+function getMin(s) {
+	var x;
+	var min = Number.POSITIVE_INFINITY;
+	for (x in s) {
+		if (s[x] < min) {
+			min = s[x];
+		}
+	}
+	return min;
 }
